bc5aadd4d41aeb1ebe3ac392776f53d99fa425e2
DreamerAbhishek <109310640+DreamerAbhishek@users.noreply.github.com>
2023-09-18 01:51:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const products_routes = require("./routes/products");
 
 app.get("/", (req, res) => {
@@ -16,7 +16,7 @@ app.use("/api/products", products_routes);
 const start = async () => {
   try {
     await connectDB(process.env.MONGODB_URL);
-    app.listen(PORT, () => {
+    app.listen(PORT,"0.0.0.0",() => {
       console.log(`${PORT} Yes I am connected`);
     });
   } catch (error) {
