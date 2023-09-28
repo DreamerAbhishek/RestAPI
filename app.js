bcc8d07aecbcd16c685520b90b87cd55dce447ba
DreamerAbhishek <abhishekma1997@gmail.com>
2023-09-29 01:01:30 +0530

    add cors in proj

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const connectDB = require("./db/connect");
 const PORT = process.env.PORT || 3000;
 const products_routes = require("./routes/products");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("that I'm live Now");
